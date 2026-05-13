227f1ef829d3e73f33041d5fc39eaf74596aeebb
Leonardo Risi <leonardorisi00@gmail.com>
2026-05-13 09:34:10 -0300

    Spring initialize
    Iniciacao do spring

---
+++ src/main/java/br/com/unitreino/UnitreinoApplication.java
@@ -0,0 +1,13 @@
+package br.com.unitreino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitreinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitreinoApplication.class, args);
+	}
+
+}
